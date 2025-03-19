eae01ee57d6e66b1a001158f4547a08c6be717f9
Bogdan <kn23-b.barylovskyi@nubip.edu.ua>
2025-03-19 09:12:10 +0200

    task

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h2>Це Footer сайту</h2>
+    </footer>
+  );
+}
+
+export default Footer;
